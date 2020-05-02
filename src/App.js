ee17a2c11712a40407181d5927f59f9cfbd684a1
Johan <johan.kruse@gmail.com>
2020-05-02 21:13:22 +0200

    Cleaned code a bit, also gave types as a prop (1 time calculation)

--- src/App.js
+++ src/App.js
@@ -1,47 +1,63 @@
 import React, {useState} from "react";
-import {Route, useRouteMatch } from 'react-router-dom'
-import { Pokemon } from './components/Pokemon'
-import { PokemonList } from './components/PokemonList'
+import {Route, useRouteMatch, useHistory, Link } from 'react-router-dom'
 
 import "./App.css";
 
-const Details = (props) => {
-  const pokemons = props.pokemons
+const Pokemon = ({ data }) => (
+  <>
+    <h1>{data.name}</h1>
+    <img src={data.img} alt={data.name} />
+  </>
+)
+
+const Details = ({ pokemons }) => {
   const match = useRouteMatch()
 
   return <Pokemon data={pokemons[match.params.id]} />
 }
 
-function App(props) {
-  const pokemons = props.pokemons
-  const [pokemonTypes, setPokemonTypes] = useState([])
-  const [search, setSearch] = useState("")
+const List = ({ pokemons }) => pokemons.map(pokemon => (
+  <div>
+    <Link to={`/list/${pokemon.id}`}>{pokemon.name}</Link>
+  </div>
+))
 
+function App({pokemons, types}) {
+  const [searchType, setSearchType] = useState([])
+  const [search, setSearch] = useState("")
+  const history = useHistory(); // we need this to change the front end router url
+
+  // This is to filter the pokemon name by search input
   const filtered = pokemons.filter(pokemon => {
     const name = pokemon.name.toLowerCase()
     const includes = name.includes(search)
     return includes
   })
 
-  const types = ["Psychic", "Flying", "Dragon", "Fire", "Electric", "Ice", "Normal", "Rock", "Water", "Bug", "Grass", "Ground", "Poison", "Fighting", "Ghost"]
-
   return (
     <>
+      <h1><Link to="/">Pokedex</Link></h1>
       <Route path="*">
-        <input type="text" onChange={e => setSearch(e.target.value)} value={search} />
-        <select onChange={(e) => setPokemonTypes(e.target.value)}>
+        <input type="text" value={search} onChange={e => {
+          history.push('/') // Send user back to path '/'.
+          setSearch(e.target.value) // update the current search value
+        }}
+        />
+        <select onChange={e => setSearchType(e.target.value)}>
           {types.map(type => (
             <option>{type}</option>
           ))}
         </select>
       </Route>
       <Route path="/" exact>
-        <PokemonList pokemons={pokemons.filter(pokemon => pokemon.type.includes(pokemonTypes))} />
+        <h2>Types:</h2>
+        <List pokemons={pokemons.filter(pokemon => pokemon.type.includes(searchType))} />
 
-        {filtered.map(pokemon => <Pokemon data={pokemon}/>)}
+        <h2>Search:</h2>
+        <List pokemons={filtered} />
       </Route>
       <Route path="/list" exact>
-        <PokemonList pokemons={pokemons}/>
+        <List pokemons={pokemons}/>
       </Route>
       <Route path="/list/:id" exact>
         <Details pokemons={pokemons} />
--- src/index.js
+++ src/index.js
@@ -5,10 +5,26 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 const pokedata = require('./pokedata.json')
 
+const types = []
+
+// we want a list of all unique pokemon types
+// loop over all the pokemons
+pokedata.pokemons.map(pokemon => {
+  // map over all the current pokemon type
+  return pokemon.type.map(type => {
+    // if our current current is not already added to types, add it.
+    if(!types.includes(type)){
+      types.push(type)
+    }
+
+    return null // need to return something
+  })
+})
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App pokemons={pokedata.pokemons} />
+      <App pokemons={pokedata.pokemons} types={types} />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
