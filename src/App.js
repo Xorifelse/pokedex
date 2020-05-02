f4eeccca5cab0856c1d232485bbf52fc76376a47
Johan <johan.kruse@gmail.com>
2020-05-02 19:55:19 +0200

    Added types

--- src/App.js
+++ src/App.js
@@ -9,36 +9,36 @@ const Details = (props) => {
   const pokemons = props.pokemons
   const match = useRouteMatch()
 
-  console.log(match.params.id)
-
   return <Pokemon data={pokemons[match.params.id]} />
 }
 
 function App(props) {
   const pokemons = props.pokemons
-  const [index, setIndex] = useState(0)
+  const [pokemonTypes, setPokemonTypes] = useState([])
   const [search, setSearch] = useState("")
 
-  console.log("search value", search)
- 
   const filtered = pokemons.filter(pokemon => {
-    console.log("name", pokemon.name)
     const name = pokemon.name.toLowerCase()
     const includes = name.includes(search)
     return includes
   })
 
-  console.log(filtered)
+  const types = ["Psychic", "Flying", "Dragon", "Fire", "Electric", "Ice", "Normal", "Rock", "Water", "Bug", "Grass", "Ground", "Poison", "Fighting", "Ghost"]
 
   return (
     <>
       <Route path="*">
         <input type="text" onChange={e => setSearch(e.target.value)} value={search} />
+        <select onChange={(e) => setPokemonTypes(e.target.value)}>
+          {types.map(type => (
+            <option>{type}</option>
+          ))}
+        </select>
       </Route>
       <Route path="/" exact>
+        <PokemonList pokemons={pokemons.filter(pokemon => pokemon.type.includes(pokemonTypes))} />
+
         {filtered.map(pokemon => <Pokemon data={pokemon}/>)}
-        
-        <button onClick={() => setIndex(index + 1)}>Next Pokemon</button>
       </Route>
       <Route path="/list" exact>
         <PokemonList pokemons={pokemons}/>
